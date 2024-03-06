5ce77e6285610d960d6aa4e9a32954508edbde21
Randy José Durán <randy.duran@tbint.de>
2024-03-06 23:32:06 +0100

    add authorization

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Althinect\FilamentSpatieRolesPermissions\Concerns\HasSuperAdmin;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
 
     use HasApiTokens, HasFactory, Notifiable, HasSuperAdmin;
@@ -41,4 +43,9 @@ class User extends Authenticatable
         'permissions'          => 'array',
         'email_verified_at'    => 'datetime',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
