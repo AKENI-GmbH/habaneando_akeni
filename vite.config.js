47895f0c01a19b4438377777a2aaf3291c33299a
Randy José Durán <randy.duran@tbint.de>
2024-03-07 09:15:24 +0100

    fixes engine

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: [
-                ...refreshPaths,
-                'app/Filament/**',
-                'app/Forms/Components/**',
-                'app/Livewire/**',
-                'app/Infolists/Components/**',
-                'app/Providers/Filament/**',
-                'app/Tables/Columns/**',
+                true
             ],
         }),
     ],
