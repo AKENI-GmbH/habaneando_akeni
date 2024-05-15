939955cd88538612f5afcd7ad5a3ed61fe0ee001
Randy José Durán <randy.duran@tbint.de>
2024-05-15 13:01:39 +0200

    update

--- resources/views/livewire/page/courseinfo.blade.php
+++ resources/views/livewire/page/courseinfo.blade.php
@@ -1,3 +1,8 @@
 <div>
     <x-hero :title="$page->name" :header="$page->header" />
+    <x-container>
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <img src="{{url('/images/programm.png') }}" alt="program" />
+        </div>
+    </x-container>
 </div>
