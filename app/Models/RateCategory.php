67271097c160e875695cb343d0c20ad204e16f5b
Randy José Durán <randy.duran@tbint.de>
2024-06-13 00:31:06 +0200

    update

--- app/Models/RateCategory.php
+++ app/Models/RateCategory.php
@@ -19,4 +19,9 @@ class RateCategory extends Model
     {
         return $this->hasMany(ClubRate::class, 'rate_category_id');
     }
+
+    public function activeRates()
+    {
+        return $this->hasMany(ClubRate::class, 'rate_category_id')->where('status', true);
+    }
 }
