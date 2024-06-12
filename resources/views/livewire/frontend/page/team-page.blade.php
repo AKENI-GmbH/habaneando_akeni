0bd5e6016bc3c66f71a75bb04079724b2dadcc32
Randy José Durán <randy.duran@tbint.de>
2024-06-12 14:34:09 +0200

    team page

--- resources/views/livewire/frontend/page/team-page.blade.php
+++ resources/views/livewire/frontend/page/team-page.blade.php
@@ -1,33 +1,18 @@
 <div>
     <x-hero :title="$page->name" :header="$page->header" />
-    <x-container>
-        <div class="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-            <div class="mx-auto max-w-2xl lg:mx-0">
-                <h2 class="text-3xl font-bold tracking-tight text-neutral-900 sm:text-4xl">Unser Team</h2>
-                <p class="mt-6 text-lg leading-8 text-neutral-600">{!! $page->body !!}</p>
-            </div>
-            <ul role="list"
-                class="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-8 xl:col-span-2">
 
+    <x-container>
+        <div class="mx-auto max-w-7xl px-6 text-center lg:px-8">
+            <ul role="list"
+                class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                 @foreach ($members->where('is_staff', true) as $member)
                     <li>
-                        <img class="aspect-[3/2] w-full rounded-2xl object-cover"
-                            src="{{$member->thumbnail}}"
+                        <img class="aspect-[3/2] w-full rounded-2xl object-cover" src="{{ $member->thumbnail }}"
                             alt="">
-                        <h3 class="mt-6 text-lg font-semibold leading-8 text-neutral-900">{{ $member->full_name }}</h3>
-                        <p class="text-base leading-7 text-neutral-600">{{ $member->styles }}</p>
-                        <p class="mt-4 text-base leading-7 text-neutral-600">{{ $member->description }}</p>
-                        <ul role="list" class="mt-6 flex gap-x-6">
-
-                            @if (isset($member->facebook))
-                                <li>
-                                    <a href="{{ $member->facebook }}" target="_blank"
-                                        class="text-neutral-400 hover:text-neutral-500">
-                                        <span class="sr-only">Facebook</span>
-                                        <x-icons.facebook />
-                                    </a>
-                                </li>
-                            @endif
+                        <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">
+                            {{ $member->full_name }}</h3>
+                        <p class="text-sm leading-6 text-gray-600">{{ $member->styles }}</p>
+                        <ul role="list" class="mt-6 flex justify-center gap-x-6">
                             @if (isset($member->instagram))
                                 <li>
                                     <a href="{{ $member->instagram }}" target="_blank"
@@ -37,29 +22,20 @@
                                     </a>
                                 </li>
                             @endif
-                            @if (isset($member->threads))
+                            @if (isset($member->facebook))
                                 <li>
-                                    <a href="{{ $member->threads }}" target="_blank"
+                                    <a href="{{ $member->facebook }}" target="_blank"
                                         class="text-neutral-400 hover:text-neutral-500">
-                                        <span class="sr-only">Threads</span>
-                                        <x-icons.threads />
-                                    </a>
-                                </li>
-                            @endif
-                            @if (isset($member->youtube))
-                                <li>
-                                    <a href="{{ $member->youtube }}" target="_blank"
-                                        class="text-neutral-400 hover:text-neutral-500">
-                                        <span class="sr-only">Youtube</span>
-                                        <x-icons.youtube />
+                                        <span class="sr-only">Facebook</span>
+                                        <x-icons.facebook />
                                     </a>
                                 </li>
                             @endif
                         </ul>
                     </li>
                 @endforeach
+                <!-- More people... -->
             </ul>
         </div>
     </x-container>
-
 </div>
