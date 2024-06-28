581eb0c8e3226ccbf0f1a495749ef5fecc2a7c20
Randy José Durán <randy.duran@tbint.de>
2024-06-28 09:28:03 +0200

    update

--- resources/views/livewire/frontend/event/event-single.blade.php
+++ resources/views/livewire/frontend/event/event-single.blade.php
@@ -79,39 +79,41 @@
                                     <h3 class="font-bold text-lg">Tickets kaufen</h3>
 
                                     @if ($errors->any())
-                                    <div class="p-3">
-                                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-                                            role="alert">
-                                            <strong class="font-bold">Achtung!</strong>
-                                            <ul>
-                                                @foreach ($errors->all() as $error)
-                                                    <li>{{ $error }}</li>
-                                                @endforeach
-                                            </ul>
-                                            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                                                <svg class="fill-current h-6 w-6 text-red-500" role="button"
-                                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                    <title>Schließen</title>
-                                                    <path
-                                                        d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                                                </svg>
-                                            </span>
+                                        <div class="p-3">
+                                            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                                                role="alert">
+                                                <strong class="font-bold">Achtung!</strong>
+                                                <ul>
+                                                    @foreach ($errors->all() as $error)
+                                                        <li>{{ $error }}</li>
+                                                    @endforeach
+                                                </ul>
+                                                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                                                    <svg class="fill-current h-6 w-6 text-red-500" role="button"
+                                                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                        <title>Schließen</title>
+                                                        <path
+                                                            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                                                    </svg>
+                                                </span>
+                                            </div>
                                         </div>
-                                    </div>
-                                @endif
+                                    @endif
 
 
                                     @if ($event->ticketType)
                                         <x-input.group for="ticket" label="Ticket wählen">
-                                            <select wire:model="ticket">
-                                                <option value="">Select a ticket</option>
+                                            <x-input.select wire:model="ticket">
+                                                <option value="">Ticket auswählen</option>
                                                 @foreach ($tickets as $ticket)
-                                                    <option value="{{ $ticket->id }}">{{ $ticket->name }} - {{ formatPriceGerman($ticket->amount) }}€</option>
+                                                    <option value="{{ $ticket->id }}">{{ $ticket->name }} -
+                                                        {{ formatPriceGerman($ticket->amount) }}€</option>
                                                 @endforeach
-                                            </select>
+                                            </x-input.select>
                                         </x-input.group>
                                         <x-input.group for="quantity" label="Menge" class="mt-4">
-                                            <x-input.text wire:model.lazy="quantity" min="1" value="1" type="number" />
+                                            <x-input.text wire:model.lazy="quantity" min="1" value="1"
+                                                type="number" />
                                         </x-input.group>
                                     @endif
 
