93c9ced1a0039e1bec5560b18eecc3a707a8b2e2
Randy Duran <randy.duran@insimia.com>
2024-11-09 09:06:16 +0100

    fix workshop

--- app/Livewire/Frontend/Workshop/WorkshopList.php
+++ app/Livewire/Frontend/Workshop/WorkshopList.php
@@ -19,7 +19,7 @@ class WorkshopList extends Component
     public function mount()
     {
         $this->events = Event::where('status', true)
-        ->where('date_to', '>', Date::now())
+        ->where('date_to', '>', Date::now(3))
         ->orderBy('date_from')
         ->where('event_type', EventTypeEnum::WORKSHOP)->get();
     }
