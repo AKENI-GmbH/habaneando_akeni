a9b86ce09826157408b4c820a68ee515917f1693
Randy Duran <randy.duran@insimia.com>
2025-02-23 09:37:06 +0100

    update

--- app/Livewire/Frontend/Checkout/CourseCheckout.php
+++ app/Livewire/Frontend/Checkout/CourseCheckout.php
@@ -29,30 +29,18 @@ class CourseCheckout extends Component
         $checkout_session = StripeCheckoutSession::retrieve($this->session_id);
         $customer = Auth::guard('customer')->user();
 
-        $existingSubscription = CourseSubscription::where([
-            'customer_id'   => $customer->id,
-            'course_id'     => $checkout_session->metadata->course_id,
-            'numberOfMen'   => $checkout_session->metadata->quantityMen,
-            'numberOfWomen' => $checkout_session->metadata->quantityWomen,
-        ])->first();
-
-        if ($existingSubscription) {
-            return;
-        }
-
         $subscription = CourseSubscription::create([
-            'customer_id'       => $customer->id,
-            'course_id'         => $checkout_session->metadata->course_id,
-            'numberOfMen'       => $checkout_session->metadata->quantityMen,
-            'numberOfWomen'     => $checkout_session->metadata->quantityWomen,
-            'amount'            => $checkout_session->amount_total / 100,
-            'subscriptionType'  => SubscriptionTypeEnum::SINGLE_PAYMENT,
-            'method'            => PaymentMethodEnum::STRIPE,
-            'payment_status'    => PaymentStatusEnum::ACCEPTED,
+            'customer_id' => $customer->id,
+            'course_id' => $checkout_session->metadata->course_id,
+            'numberOfMen' => $checkout_session->metadata->quantityMen,
+            'numberOfWomen' => $checkout_session->metadata->quantityWomen,
+            'amount' => $checkout_session->amount_total / 100,
+            'subscriptionType' => SubscriptionTypeEnum::SINGLE_PAYMENT,
+            'method' => PaymentMethodEnum::STRIPE,
+            'payment_status' => PaymentStatusEnum::ACCEPTED
         ]);
 
-        Mail::to($checkout_session->customer_email)
-            ->send(new PurchaseConfirmationEmail($subscription));
+        Mail::to($checkout_session->customer_email)->send(new PurchaseConfirmationEmail($subscription));
     }
 
     public function render()
