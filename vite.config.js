a7b166a91ab0b23774d0d88bf4eaa894a6011b0d
nilskessler <nk@nkit.de>
2026-06-17 16:21:54 +0200

    dev config

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,26 @@ export default defineConfig({
     server: {
         host: "0.0.0.0",
         port: 5173,
+        strictPort: true,
+        watch: {
+            ignored: ["**/storage/**", "**/bootstrap/cache/**"],
+        },
         hmr: {
-            host: "localhost",
+            host: "dev05.akeni.net",
+            clientPort: 8087,
+            path: "/vite-hmr",
         },
     },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
+            refresh: [
+                "resources/views/**",
+                "resources/js/**",
+                "resources/css/**",
+                "routes/**",
+                "app/Livewire/**",
+            ],
         }),
     ],
 });
